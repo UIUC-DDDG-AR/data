8198de7768fef79e1224c78ec225fced0319975c
Xuehao Wang <48540959+aiB0ld@users.noreply.github.com>
2020-04-05 14:55:55 -0500

    Updated the schema and changed from dict to array

--- Code/convert2Json.py
+++ Code/convert2Json.py
@@ -6,49 +6,49 @@ path_Software_capability = "/Users/scottwang/Desktop/AR/data/csv/software_capabi
 path_Hardware_information = "/Users/scottwang/Desktop/AR/data/csv/hardware_information.csv"
 path_Software_information = "/Users/scottwang/Desktop/AR/data/csv/software_information.csv"
 
-data_capability = {}
+data_capability = []
 with open(path_capability) as csv_capability:
     cap = csv.DictReader(csv_capability)
     for csvRow in cap:
-        capability = csvRow["Capability"]
-        data_capability[capability] = csvRow
+        capability = csvRow["key"]
+        data_capability.append(csvRow)
 with open("/Users/scottwang/Desktop/AR/data/Capability/capability_information.json", 'w') as f1:
     f1.write(json.dumps(data_capability,indent=4))
 
-data_H_capability = {}
+data_H_capability = []
 with open(path_Hardware_capability) as csv_hardWare:
     hd = csv.DictReader(csv_hardWare)
     for csvRow in hd:
-        hardware = csvRow["Hardware"]
-        data_H_capability[hardware] = csvRow
+        hardware = csvRow["name"]
+        data_H_capability.append(csvRow)
 with open("/Users/scottwang/Desktop/AR/data/Hardware/hardware_capability.json", 'w') as f1:
     f1.write(json.dumps(data_H_capability,indent=4))
 
-data_S_capability = {}
+data_S_capability = []
 with open(path_Software_capability) as csv_softWare:
     st = csv.DictReader(csv_softWare)
     for csvRow in st:
-        software = csvRow["Software"]
-        data_S_capability[software] = csvRow
+        software = csvRow["name"]
+        data_S_capability.append(csvRow)
 with open("/Users/scottwang/Desktop/AR/data/Software/software_capability.json", 'w') as f1:
     f1.write(json.dumps(data_S_capability,indent=4))
 
-data_H_information = {}
+data_H_information = []
 with open(path_Hardware_information) as csv_H_capability:
     h_cap = csv.DictReader(csv_H_capability)
     for csvRow in h_cap:
-        H_capability = csvRow["Hardware"]
-        data_H_information[H_capability] = csvRow
-with open("/Users/scottwang/Desktop/AR/data/Hardware/hardware_information.json", 'w') as f1:
+        H_capability = csvRow["name"]
+        data_H_information.append(csvRow)
+with open("/Users/scottwang/Desktop/AR/data/Hardware/hardware_documentation.json", 'w') as f1:
     f1.write(json.dumps(data_H_information,indent=4))
 
-data_S_information = {}
+data_S_information = []
 with open(path_Software_information) as csv_S_capability:
     s_cap = csv.DictReader(csv_S_capability)
     for csvRow in s_cap:
         S_capability = csvRow["name"]
-        data_S_information[S_capability] = csvRow
-with open("/Users/scottwang/Desktop/AR/data/Software/software_information.json", 'w') as f1:
+        data_S_information.append(csvRow)
+with open("/Users/scottwang/Desktop/AR/data/Software/software_documentation.json", 'w') as f1:
     f1.write(json.dumps(data_S_information,indent=4))
 
 
