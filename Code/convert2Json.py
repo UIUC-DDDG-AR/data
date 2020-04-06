9a35a509032615e07aeeb16af2209df5e0898dcd
Xuehao Wang <48540959+aiB0ld@users.noreply.github.com>
2020-04-05 23:25:12 -0500

    Change the type and schma based on doc

--- Code/convert2Json.py
+++ Code/convert2Json.py
@@ -10,7 +10,6 @@ data_capability = []
 with open(path_capability) as csv_capability:
     cap = csv.DictReader(csv_capability)
     for csvRow in cap:
-        capability = csvRow["key"]
         data_capability.append(csvRow)
 with open("/Users/scottwang/Desktop/AR/data/Capability/capability_information.json", 'w') as f1:
     f1.write(json.dumps(data_capability,indent=4))
@@ -19,8 +18,17 @@ data_H_capability = []
 with open(path_Hardware_capability) as csv_hardWare:
     hd = csv.DictReader(csv_hardWare)
     for csvRow in hd:
-        hardware = csvRow["name"]
         data_H_capability.append(csvRow)
+for item in data_H_capability:
+    # print (type(item.values()))
+    for key in item:
+        if item[key][:3] == 'yes':
+            item[key] = (1,item[key][5:])
+        if item[key][:2] == 'no':
+            item[key] = (0,item[key][4:])
+        if item[key][:8] == 'possible':
+            item[key] = (2,item[key][10:])
+    
 with open("/Users/scottwang/Desktop/AR/data/Hardware/hardware_capability.json", 'w') as f1:
     f1.write(json.dumps(data_H_capability,indent=4))
 
@@ -28,8 +36,17 @@ data_S_capability = []
 with open(path_Software_capability) as csv_softWare:
     st = csv.DictReader(csv_softWare)
     for csvRow in st:
-        software = csvRow["name"]
         data_S_capability.append(csvRow)
+
+for item in data_S_capability:
+    for key in item:
+        if item[key][:3] == 'yes':
+            item[key] = (1,item[key][5:])
+        if item[key][:2] == 'no':
+            item[key] = (0,item[key][4:])
+        if item[key][:8] == 'possible':
+            item[key] = (2,item[key][10:])
+
 with open("/Users/scottwang/Desktop/AR/data/Software/software_capability.json", 'w') as f1:
     f1.write(json.dumps(data_S_capability,indent=4))
 
@@ -37,8 +54,16 @@ data_H_information = []
 with open(path_Hardware_information) as csv_H_capability:
     h_cap = csv.DictReader(csv_H_capability)
     for csvRow in h_cap:
-        H_capability = csvRow["name"]
         data_H_information.append(csvRow)
+for item in data_H_information:
+    if (item['field_of_view'] != ''):
+        item['field_of_view'] = int(item['field_of_view']) 
+    item['use_cases'] = item['use_cases'].split(',')
+    item['related_softwares'] = item['related_softwares'].split(',')
+    item['related_hardwares'] = item['related_hardwares'].split(',')
+    item['customer_segment'] = item['customer_segment'].split(',')
+    item['possible_interactions'] = item['possible_interactions'].split(',')
+
 with open("/Users/scottwang/Desktop/AR/data/Hardware/hardware_documentation.json", 'w') as f1:
     f1.write(json.dumps(data_H_information,indent=4))
 
@@ -46,8 +71,12 @@ data_S_information = []
 with open(path_Software_information) as csv_S_capability:
     s_cap = csv.DictReader(csv_S_capability)
     for csvRow in s_cap:
-        S_capability = csvRow["name"]
         data_S_information.append(csvRow)
+for item in data_S_information:
+    item['related_softwares'] = item['related_softwares'].split(',')
+    item['related_hardwares'] = item['related_hardwares'].split(',')   
+    item['key_features'] = item['key_features'].split(',')
+        
 with open("/Users/scottwang/Desktop/AR/data/Software/software_documentation.json", 'w') as f1:
     f1.write(json.dumps(data_S_information,indent=4))
 
