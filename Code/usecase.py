c80a6ccfb8f8605d0cb89cbb95108930900546cd
Xuehao Wang <48540959+aiB0ld@users.noreply.github.com>
2020-07-11 22:30:39 -0500

    transpose usecase json

--- Code/usecase.py
+++ Code/usecase.py
@@ -1,14 +1,15 @@
 import csv, json 
 
-path_usecase = "csv/usecase.csv"
+path_usecase = "csv/usecase_transpose.csv"
 
 usecase = []
+# array = []
 with open(path_usecase) as csv_usecase:
     uc = csv.DictReader(csv_usecase)
     for csvRow in uc:
         usecase.append(csvRow)
 for item in usecase:
-    # print (type(item.values()))
+    # print (type(item))
     for key in item:
         if item[key][:3] == 'yes':
             item[key] = (1,item[key][5:])
@@ -16,6 +17,6 @@ for item in usecase:
             item[key] = (0,item[key][4:])
         if item[key][:8] == 'possible':
             item[key] = (2,item[key][10:])
-    
-with open("usecase.json", 'w') as f1:
+
+with open("Usecase/usecase_transpose.json", 'w') as f1:
     f1.write(json.dumps(usecase,indent=4))
