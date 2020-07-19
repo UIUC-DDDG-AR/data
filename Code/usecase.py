d74dd3ee0d4fbfc05a9919fe919a41d63ab75952
Xuehao Wang <48540959+aiB0ld@users.noreply.github.com>
2020-07-19 14:44:37 -0500

    add example page card content

--- Code/usecase.py
+++ Code/usecase.py
@@ -1,6 +1,6 @@
 import csv, json 
 
-path_usecase = "csv/usecase_transpose.csv"
+path_usecase = "csv/example_page_content.csv"
 
 usecase = []
 # array = []
@@ -8,15 +8,15 @@ with open(path_usecase) as csv_usecase:
     uc = csv.DictReader(csv_usecase)
     for csvRow in uc:
         usecase.append(csvRow)
-for item in usecase:
-    # print (type(item))
-    for key in item:
-        if item[key][:3] == 'yes':
-            item[key] = (1,item[key][5:])
-        if item[key][:2] == 'no':
-            item[key] = (0,item[key][4:])
-        if item[key][:8] == 'possible':
-            item[key] = (2,item[key][10:])
+# for item in usecase:
+#     # print (type(item))
+#     for key in item:
+#         if item[key][:3] == 'yes':
+#             item[key] = (1,item[key][5:])
+#         if item[key][:2] == 'no':
+#             item[key] = (0,item[key][4:])
+#         if item[key][:8] == 'possible':
+#             item[key] = (2,item[key][10:])
 
-with open("Usecase/usecase_transpose.json", 'w') as f1:
+with open("example_page_content.json", 'w') as f1:
     f1.write(json.dumps(usecase,indent=4))
